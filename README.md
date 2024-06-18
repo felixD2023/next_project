8cc98dcd88d4e8a4c38cd37d16f64a09b904a2de
felix <felixdarielhernandezperez@gmail.com>
2024-06-17 22:01:56 -0400

    Unico commit

---
+++ README.md
@@ -0,0 +1,7 @@
+<h1>Optativa de Next</h1>
+<h4>Integrantes, Grupo 403</h4>
+<ul>
+<li>Osmani Serrano Herrera</li>
+<li>Felix Dariel Hernández Pérez</li>
+<li>Magela Isaura Jaime Toledano</li>
+</ul>
---
+++ src/pages/index.js
@@ -0,0 +1,33 @@
+import ProductCard from '@/components/products/card';
+import Banner from '@/components/banner';
+
+const URL_BASE = process.env.URL_BASE;
+
+export async function getServerSideProps() {
+  try {
+    const res = await fetch(`${URL_BASE}/api/products`);
+    const data = await res.json();
+    return { props: { data } };
+  } catch (error) {
+    return { props: { data: [] } };
+  }
+}
+
+export default function Home({data}) {
+  return (
+    <>
+      <Banner />
+      <h3 className='text-2xl font-bold text-white mb-4'>
+        Nuestros productos
+      </h3>
+      <section className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+        {data.length === 0 && (
+          <p className='text-white'>No hay productos disponibles</p>
+        )}
+        {data.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </section>
+    </>
+  )
+}
